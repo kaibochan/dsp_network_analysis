00523bf97afd88284b68802fcc19822e73634170
kaibochan <kai.buckhalter@gmail.com>
2025-11-07 02:01:12 -0500

    reading and plotting recipe network

--- recipe_network/main.py
+++ recipe_network/main.py
@@ -2,7 +2,9 @@ from network_factory import RecipeNetwork
 
 def main():
     recipe_network = RecipeNetwork("recipes_json/")
-    recipe_network.import_network_from_json("items.json")
+    recipe_network.import_network_from_json("items.json", "buildings.json")
+
+    recipe_network.plot_network()
 
 if __name__ == "__main__":
     main()
--- recipe_network/network_factory.py
+++ recipe_network/network_factory.py
@@ -1,7 +1,9 @@
 import time
 import enum
 import json
+
 import igraph
+import matplotlib.pyplot as plt
 
 class LogLevel(enum.Enum):
     TRACE = 1
@@ -26,7 +28,7 @@ class RecipeNetwork:
         self.recipe_json_path = recipe_json_path
         self.log_path = log_path
         self.log_name = log_name
-        self.network = None
+        self.network = igraph.Graph(directed=True)
 
     def _logger(self, level: LogLevel, message: str, reset: bool = False) -> None:
         if not level.value & log_level:
@@ -40,22 +42,61 @@ class RecipeNetwork:
         with open(f"{self.log_path}{self.log_name}", write_mode) as log_file:
             log_file.write(f"{now}: [{level.name}] - {message}\n")
     
-    def import_network_from_json(self, filename: str) -> None:
-        network_data = None
+    def import_network_from_json(self, *filenames: str) -> None:
+        # read json for recipes generated from data_manipulation module
+        # converts json into graph structure using igraph module
+        # this graph can then be saved into a .gml file to skip the json parsing step later
 
-        try:
-            with open(f"{self.recipe_json_path}{filename}", "r") as network_file:
-                network_data = json.load(network_file)
-                assert network_data is not None
-        except Exception as e:
-            self._logger(LogLevel.ERROR, f"Failed to parse json from file: {filename}. Error: {e}")
-            return
-        self._logger(LogLevel.INFO, f"Finished parsing json from file: {filename}")
+        file_data = []
+        for filename in filenames:
+            try:
+                with open(f"{self.recipe_json_path}{filename}", "r") as network_file:
+                    file_data.append(json.load(network_file))
+                    assert file_data[-1] is not None
+            except Exception as e:
+                self._logger(LogLevel.ERROR, f"Failed to parse json from file: {filename}. Error: {e}")
+                return
+            self._logger(LogLevel.INFO, f"Finished parsing json from file: {filename}")
+        
+        network_data = []
+        for data in file_data:
+            network_data.extend(data)
+            
+        self._logger(LogLevel.INFO, f"Converting json data into igraph network")
+
+        edges = []
+        edge_attributes = {
+            "quantity" : []
+        }
 
         for data in network_data:
-            data: dict
             name = data.get("product")
-            dependencies = data.get("ingredients")
-            self._logger(LogLevel.TRACE, f"Found {name} in network_data")
+            self.network.add_vertex(name, label=name)
 
-            
+            dependencies = data.get("ingredients")
+            for ingredient, quantity in dependencies.items():
+                try: self.network.vs.find(name=ingredient)
+                except: self.network.add_vertex(ingredient, label=ingredient)
+
+
+                edges.append((name, ingredient))
+                edge_attributes["quantity"].append(quantity)
+        self.network.add_edges(edges, edge_attributes)
+
+    def plot_network(self):
+        fig = plt.figure(0)
+        fig.add_subplot()
+        igraph.plot(
+            self.network, 
+            target=fig.axes[0],
+            vertex_size=10,
+            vertex_label_size=7,
+            vertex_label_dist=-1.5,
+            edge_width=1,
+            edge_arrow_width=5,
+            edge_arrow_size=5,
+            layout=self.network.layout("fruchterman_reingold")
+        )
+
+        plt.tight_layout()
+        plt.show()
